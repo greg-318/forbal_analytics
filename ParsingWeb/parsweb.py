59e666ef3e4cb509aa2047d2200adf4e5a0ab889
gogi-biker <biker92gogi@mail.ru>
2020-04-10 16:15:38 +0300

    Добавил файл parseweb.py в котором написал парсер для стягивания информации по игрокам. Добавил фаил chromedriver.exe для работы с selenium. В req.tex добавил две бибиолеки которые использовал в parsweb.py.
    Добавил перехват ошибок/ добавил еще одну цункцию в целях сокращения кода.

--- ParsingWeb/parsweb.py
+++ ParsingWeb/parsweb.py
@@ -1,51 +1,46 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import logging
 
 
 def data(url):
-
+    """парсинг данных футболистов,
+    url принимает ссылку на нужную страницу сайта """
     driver = webdriver.ChromeOptions()
     driver.add_argument('headless')  # запрет на открытие барузера
     driver.add_argument('window-size=1920x935')  # запрет на открытие барузера
-    driver = webdriver.Chrome(chrome_options=driver)  # запрет на открытие барузера
-
+    driver = webdriver.Chrome(chrome_options=driver)  # запускаеться веб драйвер и передаються настройки в хром .
     driver.get(url)
-    driver.find_element_by_xpath("//div[@class='players margin-top jTable']/div[@class='table-control-panel']").click()  #раскрывает дополнительную таблицу
-    click_flag = driver.find_elements_by_xpath("//div[@class='players margin-top jTable']//div[@class='table-popup']"
-                                               "//div[@class='table-popup-body']//div[@class='table-options']"
-                                               "//div[@class='table-options-row']/div[@class='row-display']")  #находит все флажки в дополнительной талице
-    j = 0
-    for i in click_flag:  # проход по всем флажкам. активация скрытых элементов основной таблицы
-        j += 1
-        if j == 7:
-            i.click()
-        if j == 12:
-            i.click()
-        if j == 14:
-            i.click()
-        if j == 15:
-            i.click()
-        if j == 17:
-            i.click()
-        if j == 19:
-            i.click()
-        if j == 20:
-            i.click()
-        if j == 21:
-            i.click()
-        if j == 22:
-            i.click()
-        if j == 23:
-            i.click()
-        if j == 24:
-            i.click()
 
-    driver.find_element_by_xpath("//div[@class='players margin-top jTable']//div[@class='table-popup']"
+    try:
+        driver.find_element_by_xpath("//div[@class='players margin-top jTable']/div[@class='table-control-panel']").click()  #раскрывает дополнительную таблицу
+    except Exception:
+        logging.warning("problems with pressing the button for additional arguments")
+        return
+
+    flags = driver.find_elements_by_xpath("//div[@class='players margin-top jTable']//div[@class='table-popup']"
+                                               "//div[@class='table-popup-body']//div[@class='table-options']"
+                                               "//div[@class='table-options-row']/div[@class='row-display']")  #находит все флажки в дополнительной таблице
+    j = 0
+    flags_position = [7, 12, 14, 15, 17, 19, 20, 21, 22, 23, 24]  # позиции фложков в дополнитерльной таблице
+    for flag in flags:  # проход по всем флажкам. активация скрытых элементов основной таблицы
+        j += 1
+        if j in flags_position:
+            flag.click()
+    try:
+        driver.find_element_by_xpath("//div[@class='players margin-top jTable']//div[@class='table-popup']"
                                  "/div[@class='table-popup-footer']/a[@class='button-apply']").click() # нажатие на кнопку apply в дополнительной таблице
+    except Exception:
+        logging.warning("Problems with clicking the checkbox activation button.")
+        return
 
     res = driver.page_source  # получение кода всей траницы. со всеми данными игроков .
     soup = BeautifulSoup(res, 'lxml')
-    scripts = soup.find('div', class_='players margin-top jTable') # получение данных таблицы игроков
+    try:
+        scripts = soup.find('div', class_='players margin-top jTable') # получение данных таблицы игроков
+    except Exception:
+        logging.warning("div-'players margin-top jTable' not found.")
+        return
 
     player = {'player': None, 'year': None, 'position': None, 'appearances': None, 'minutes': None, 'goals': None,
                   'npg': None, 'a': None, 'sh90': None, 'kp90': None, 'xg': {'value': None, "dynamic": None},
@@ -56,63 +51,10 @@ def data(url):
     table = scripts.find('tbody')  #данные таблицы игроков
     elem = []  # Будут сохраняться все данные одного игрока
     sending = {}  # Будут сохраняться только те данные игрока у которых значения не равны нулю готовые для отправки в БД.
+
     for i in table:
         for j in i:
             elem.append(j.text)
-        if '-' in elem[10]:        # получение данных степени
-            f = elem[10]
-            for z in f:
-                if z.isdigit():
-                    g = f.split('-')
-                    player['xg']['value'] = g[0]
-                    player['xg']['dynamic'] = f"-{g[1]}"
-                break
-
-        if '+' in elem[10]:     # получение данных степени
-            f = elem[10]
-            for z in f:
-                if z.isdigit():
-                    g = f.split('+')
-                    player['xg']['value'] = g[0]
-                    player['xg']['dynamic'] = f"+{g[1]}"
-                    break
-
-        if '-' in elem[11]:   # получение данных степени
-            f = elem[11]
-            for z in f:
-                if z.isdigit():
-                    g = f.split('-')
-                    player['NPxG']['value'] = g[0]
-                    player['NPxG']['dynamic'] = f"-{g[1]}"
-                break
-
-        if '+' in elem[11]:   # получение данных степени
-            f = elem[11]
-            for z in f:
-                if z.isdigit():
-                    g = f.split('+')
-                    player['NPxG']['value'] = g[0]
-                    player['NPxG']['dynamic'] = f"+{g[1]}"
-                break
-
-        if '-' in elem[12]:   # получение данных степени
-            f = elem[12]
-            for z in f:
-                if z.isdigit():
-                    g = f.split('-')
-                    player['xa']['value'] = g[0]
-                    player['xa']['dynamic'] = f"-{g[1]}"
-                    break
-
-        if '+' in elem[12]:   # получение данных степени
-            f = elem[12]
-            for z in f:
-                if z.isdigit():
-                    g = f.split('+')
-                    player['xa']['value'] = g[0]
-                    player['xa']['dynamic'] = f"+{g[1]}"
-                    break
-
         player['player'] = f"{elem[1]}_{url[-4:]}"  # доббавление к имени игрока год
         player['position'] = elem[2]
         player['appearances'] = int(elem[3])
@@ -122,9 +64,6 @@ def data(url):
         player['a'] = int(elem[7])
         player['sh90'] = float('{:.2f}'.format(float(elem[8])))  # сокрашение цифр после точки до двух символов
         player['kp90'] = float('{:.2f}'.format(float(elem[9])))
-        #player['xg'] = elem[10]   # dict
-        #player['NPxG'] = elem[11]   #dict
-        #player['xa'] = elem[12] # dict
         player['xgchain'] = float('{:.2f}'.format(float(elem[13])))
         player['xgbuildup'] = float('{:.2f}'.format(float(elem[14])))
         player['xg90'] = float('{:.2f}'.format(float(elem[15])))
@@ -136,21 +75,30 @@ def data(url):
         player['xgbuildup90'] = float('{:.2f}'.format(float(elem[21])))
         player['yellow'] = int(elem[22])
         player['red'] = int(elem[23])
-
-        for key, item in player.items():  # отбирет только те данные у которых значения не равны нулю. и вставляет в словарь sending для отправки в бд.
-            if item is None:
+        if elem[10] != None:  # если сетепрь не равен нулю то он отправляеться в базу данных .
+            player['xg'] = power(elem[10])
+        else:
+            continue
+        if elem[11] != None: # если сетепрь не равен нулю то он отправляеться в базу данных .
+            player['NPxG'] = power(elem[10])
+        else:
+            continue
+        if elem[11] != None: # если сетепрь не равен нулю то он отправляеться в базу данных .
+            player['xa'] = power(elem[10])
+        else:
+            continue
+        for key, value in player.items():  # отбирет только те данные у которых значения не равны нулю. и вставляет в словарь sending для отправки в бд.
+            if value == 0 or None:
                 continue
-            if item == 0:
-                continue
-            if type(item) == dict:  # проверка значений элементов степени, на ноль .
-                for z, x in item.items():
+            if isinstance(value, dict):  # проверка значений элементов степени, на ноль .
+                for z, x in value.items():
                     if x is not None:
-                        sending[key] = item
+                        sending[key] = value
                     else:
                         continue
             else:
-                sending[key] = item
-        print(sending)
+                sending[key] = value
+        logging.warning(sending)
         sending = {}
         elem = []
 
@@ -162,35 +110,46 @@ def data(url):
                   'xgchain90': None, 'xgbuildup90': None, 'yellow': None, 'red': None}  # присвоение всем ззначения None
 
 
+def power(value):
+    """ value принимает данные со степеью
+    функция возвращает два значения, значение числа и его степень """
+    if '-' in value:  # получение данных степени
+        f = value
+        for z in f:
+            if z.isdigit():
+                g = f.split('-') #данные получаюстья в виде 1,02-2,03 . в этой строке данные деляться на две чсти
+                value = float(g[0])
+                dynamic = f"-{g[1]}"
+            break
+        return {'value': value, 'dynamic' : dynamic}
+    if '+' in value:  # получение данных степени
+        f = value
+        for z in f:
+            if z.isdigit():
+                g = f.split('+')
+                value = float(g[0])
+                dynamic = f"+{g[1]}"
+            break
+        return {'value': value, 'dynamic' : dynamic}
+
+
 def web():
     """получение ссылок команд"""
     url = 'https://understat.com/team/'
-    team = ["Augsburg","Bayer Leverkusen", "Bayern Munich", "Borussia Dortmund",
+    teams = ("Augsburg","Bayer Leverkusen", "Bayern Munich", "Borussia Dortmund",
     "Borussia M.Gladbach", "Darmstadt" ,"Eintracht Frankfurt","FC Cologne",
     "Fortuna Duesseldorf","Freiburg","Hamburger SV","Hannover 96","Hertha Berlin",
     "Hoffenheim","Ingolstadt","Mainz 05","Nuernberg","Paderborn","RasenBallsport Leipzig",
-    "Schalke 04","Union Berlin","VfB Stuttgart","Werder Bremen","Wolfsburg"]
+    "Schalke 04","Union Berlin","VfB Stuttgart","Werder Bremen","Wolfsburg")
 
-    yar = ['2014','2015','2016','2017','2018','2019']
+    years = ('2014', '2015', '2016', '2017', '2018', '2019')
 
-    for i in yar:
-        for j in team:
-            http = url+j+'/'+i
+    for year in years:
+        for team in teams:
+            http = url+team+'/'+year
             data(http)
 
 
 if __name__ == '__main__':
 
     web()
-
-
-
-
-
-
-
-
-
-
-
-
