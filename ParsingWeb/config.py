3cd9b117211247f72d91c0595a79153c1216c14e
gogi-biker <biker92gogi@mail.ru>
2020-04-24 21:59:40 +0300

    Добавил файл parseweb.py в котором написал парсер для стягивания информации по игрокам. Добавил фаил chromedriver.exe для работы с selenium. В req.tex добавил две бибиолеки которые использовал в parsweb.py.
    Добавил перехват ошибок/ добавил еще одну цункцию в целях сокращения кода.
    
    допилил перехват ошибок . поменял место обьявления обьекта класс
    
    Добавил еще одну функцию для отправки данных в базу данных. и установил
    рестарт программы через 4 дня.
    также добавил еще один файл в который перенес три переменные с именами
    команд, основную ссылку, и годы.

---
+++ ParsingWeb/config.py
@@ -0,0 +1,9 @@
+
+url = 'https://understat.com/team/'
+teams = ("Augsburg", "Bayer Leverkusen", "Bayern Munich", "Borussia Dortmund",
+         "Borussia M.Gladbach", "Darmstadt", "Eintracht Frankfurt", "FC Cologne",
+         "Fortuna Duesseldorf", "Freiburg", "Hamburger SV", "Hannover 96", "Hertha Berlin",
+         "Hoffenheim", "Ingolstadt", "Mainz 05", "Nuernberg", "Paderborn", "RasenBallsport Leipzig",
+         "Schalke 04", "Union Berlin", "VfB Stuttgart", "Werder Bremen", "Wolfsburg")
+
+years = ('2014', '2015', '2016', '2017', '2018', '2019')
--- ParsingWeb/parsweb.py
+++ ParsingWeb/parsweb.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.common.exceptions import NoSuchElementException
+import time
+from ParsingWeb.config import url, teams, years
 import logging
-from DataBase.models.player import Player
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+import sys
+sys.path.append("..\DataBase")
+from models.player import Player
+from mongo_default import MongoDefault
 
 
 def click(url):
@@ -65,51 +71,62 @@ def power(string):
     return {'value': float(values[0]), "dynamic": symbol+values[1]}
 
 
+def send(data, player='players'):
+    """
+    :param data: - словарь данных для отправки в базу данных
+    :param player: - название базы данных
+    :return: - ошибок вставки данных в бд, или открытия класса
+    """
+    try:
+        with MongoDefault(player) as instance:
+            answer = instance.insert(data)
+            if 'Error' in answer:
+                logging.warning('Key already created, did not manage to send data to the database.')
+    except ConnectionError:
+        logging.warning('Could not contact class MongoDefault')
+
+
 if __name__ == '__main__':
 
-    url = 'https://understat.com/team/'
-    teams = ("Augsburg", "Bayer Leverkusen", "Bayern Munich", "Borussia Dortmund",
-    "Borussia M.Gladbach", "Darmstadt", "Eintracht Frankfurt", "FC Cologne",
-    "Fortuna Duesseldorf", "Freiburg", "Hamburger SV", "Hannover 96", "Hertha Berlin",
-    "Hoffenheim", "Ingolstadt", "Mainz 05", "Nuernberg", "Paderborn", "RasenBallsport Leipzig",
-    "Schalke 04", "Union Berlin", "VfB Stuttgart", "Werder Bremen", "Wolfsburg")
-
-    years = ('2014', '2015', '2016', '2017', '2018', '2019')
-    for year in years:
-        for team in teams:
-            http = url+team+'/'+year
-            parser = click(http)
-            if not parser:
-                continue
-            soup = pars(parser)
-            if not soup:
-                continue
-            for i in soup:
-                information = Player()
-                elem = tuple(j.text for j in i)
-                information.player = f"{http[-4:]}_{elem[1]}"
-                information.position = elem[2]
-                information.appearances = int(elem[3])
-                information.minutes = int(elem[4])
-                information.goals = int(elem[5])
-                information.npg = int(elem[6])
-                information.a = int(elem[7])
-                information.sh90 = float('{:.2f}'.format(float(elem[8])))
-                information.kp90 = float('{:.2f}'.format(float(elem[9])))
-                information.xgchain = float('{:.2f}'.format(float(elem[13])))
-                information.xgbuildup = float('{:.2f}'.format(float(elem[14])))
-                information.xg90 = float('{:.2f}'.format(float(elem[15])))
-                information.npxg90 = float('{:.2f}'.format(float(elem[16])))
-                information.xa90 = float('{:.2f}'.format(float(elem[17])))
-                information.xg90xa90 = float('{:.2f}'.format(float(elem[18])))
-                information.npxg90xa90 = float('{:.2f}'.format(float(elem[19])))
-                information.xgchain90 = float('{:.2f}'.format(float(elem[20])))
-                information.xgbuildup90 = float('{:.2f}'.format(float(elem[21])))
-                information.yellow = int(elem[22])
-                information.red = int(elem[23])
-                if elem[10] != '0.00':
-                    information.xg = power(elem[10])
-                if elem[11] != '0.00':
-                    information.npxg = power(elem[11])
-                if elem[12] != '0.00':
-                    information.xa = power(elem[12])
+    while True:
+        for year in years:
+            for team in teams:
+                http = url+team+'/'+year
+                parser = click(http)
+                if not parser:
+                    continue
+                soup = pars(parser)
+                if not soup:
+                    continue
+                for i in soup:
+                    information = Player()
+                    elem = tuple(j.text for j in i)
+                    information.player = f"{http[-4:]}_{elem[1]}"
+                    information.position = elem[2]
+                    information.appearances = int(elem[3])
+                    information.minutes = int(elem[4])
+                    information.goals = int(elem[5])
+                    information.npg = int(elem[6])
+                    information.a = int(elem[7])
+                    information.sh90 = float('{:.2f}'.format(float(elem[8])))
+                    information.kp90 = float('{:.2f}'.format(float(elem[9])))
+                    information.xgchain = float('{:.2f}'.format(float(elem[13])))
+                    information.xgbuildup = float('{:.2f}'.format(float(elem[14])))
+                    information.xg90 = float('{:.2f}'.format(float(elem[15])))
+                    information.npxg90 = float('{:.2f}'.format(float(elem[16])))
+                    information.xa90 = float('{:.2f}'.format(float(elem[17])))
+                    information.xg90xa90 = float('{:.2f}'.format(float(elem[18])))
+                    information.npxg90xa90 = float('{:.2f}'.format(float(elem[19])))
+                    information.xgchain90 = float('{:.2f}'.format(float(elem[20])))
+                    information.xgbuildup90 = float('{:.2f}'.format(float(elem[21])))
+                    information.yellow = int(elem[22])
+                    information.red = int(elem[23])
+                    if elem[10] != '0.00':
+                        information.xg = power(elem[10])
+                    if elem[11] != '0.00':
+                        information.npxg = power(elem[11])
+                    if elem[12] != '0.00':
+                        information.xa = power(elem[12])
+                    players = information.dict()
+                    send(players)
+        time.sleep(345600)
