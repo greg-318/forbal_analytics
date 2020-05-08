efd6e14f2ecd89ca2596c49111227120dbe12826
gogi-biker <biker92gogi@mail.ru>
2020-05-09 00:24:14 +0300

    Перенес большую часть одной функции в if main .

--- ParsingWeb/parsgame.py
+++ ParsingWeb/parsgame.py
@@ -1,25 +1,18 @@
+import re
+import time
+import logging
 from selenium import webdriver
 from bs4 import BeautifulSoup
-from selenium.common.exceptions import NoSuchElementException
-import time
-from ParsingWeb.config import url, years, ligues
-import logging
-import requests
-import re
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
-import sys
-#sys.path.append("..\DataBase")
-#from models.player import Player
-#from mongo_default import MongoDefault
-#import sys
-sys.path.append("..\ParsingWeb")
+from config import url, years, ligues
 from parsweb import lig
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 
 
 def pars(html):
     """
     :param html: - получает html код страницы .
-    :return: - возвращает списко где первый элемент это название команды чьи значения указаны с левой стороны таблицы
+    :ball_possesion: - получает три значения changes .
+    :return: - возвращает список где первый элемент это название команды чьи значения указаны с левой стороны таблицы
      2 и 3 пренадлежат все тожей команже а 4 и 5 команде которая с права . Последний элемент списка это список значений
      changes.
     """
@@ -30,82 +23,59 @@ def pars(html):
         logging.warning('The name of the team that plays at home was not found.')
         return None
     indicators.append(name_team_home[1:-1])
-    divas = (7, 9, 8, 10)
-    for div in divas:
-        result = soup.find_all('div', class_='progress-value')[div].get_text()
+    values = (7, 9, 8, 10)
+    for value in values:
+        result = soup.find_all('div', class_='progress-value')[value].get_text()
         if not result:
-            logging.warning('team game values ​​not found.')
-            return None
-        elif '.'in result:
+            logging.warning('team game values not found.')
+            continue
+        if '.'in result:
             indicators.append(float(result))
         else:
             indicators.append(int(result))
     result = soup.find_all('div', class_='progress-bar')[1]
-    teams = re.findall(r'title="([\d]+)', str(result))
-    indicators.append(teams)
+    ball_possesion = re.findall(r'title="([\d]+)', str(result)) # пока не работаем с ними.
+    indicators.append(ball_possesion)
     return indicators
-
-
-def click(http):
-    """
-    заходит на все матчи данной команды и складывает все значения xg и shots всех матчей .
-    :param http: ссылка страницы команды
-    :return: возвращает список где первые два значения это значения данной команды а остальные две значения команд
-    с которыми команда играла .
-    """
-    driver = webdriver.ChromeOptions()
-    driver.add_argument('headless')
-    driver.add_argument('window-size=1920x935')
-    driver = webdriver.Chrome(chrome_options=driver)
-    driver.get(http)
-    logging.info('getting page function, click')
-    try:
-        quantity = driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[2]/div/div[2]/div[1]/div").find_elements_by_tag_name("a")
-        links = tuple(x.get_attribute("href") for x in quantity if "match" in x.get_attribute("href"))
-    except NoSuchElementException:
-        logging.warning("It is not possible to get links of all matches of a team.", http)
-        return
-    name_team = re.findall(r'team/([\w]+)', str(http))
-
-    result = [0, 0, 0, 0]
-    for link in links:
-        driver.get(link)
-        try:
-            driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[2]/div[1]/div/div[1]/div/label[3]').click()
-        except NoSuchElementException:
-            logging.warning("It’s not possible to click on the disclosure button of the game indicators.", http)
-            return
-        time.sleep(1)
-        pes = driver.page_source
-        if not pes:
-            logging.warning('не получаю кода страницы для нажатия кнопки')
-        page = pars(pes)
-        if not page:
-            logging.warning('функуия pars не возвращает данные ')
-            continue
-        if name_team == page[0]:
-            result[0] += page[1]
-            result[1] += page[2]
-            result[2] += page[3]
-            result[3] += page[4]
-        else:
-            result[0] += page[3]
-            result[1] += page[4]
-            result[2] += page[1]
-            result[3] += page[2]
-    return result
 
 
 if __name__ == '__main__':
 
-    for ligue in ligues:
-        teams = lig(ligue)
-        if not teams:
-            continue
-        for year in years:
-            for team in teams:
-                http = url + team + '/' + year
-                parser = click(http)
-                if not parser:
-                    continue
-                logging.info(parser)
+    while True:
+        for ligue in ligues:
+            teams = lig(ligue)
+            for year in years:
+                for team in teams:
+                    http = url + team + '/' + year
+                    driver = webdriver.ChromeOptions()
+                    driver.add_argument('headless')
+                    driver.add_argument('window-size=1920x935')
+                    driver = webdriver.Chrome(chrome_options=driver)
+                    driver.get(http)
+                    quantity = driver.find_elements_by_tag_name("a")
+                    links = tuple(x.get_attribute("href")for x in quantity if "match" in str(x.get_attribute("href")))
+                    if not links:
+                        logging.warning("not possible to find team matches")
+                        continue
+                    name_team = re.findall(r'team/([\w]+)', str(http))  # название домашней команды .
+                    data = [0, 0, 0, 0]
+                    for link in links:
+                        driver.get(link)
+                        driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[2]/div[1]/div/div[1]/div/label[3]'
+                                                     ).click()
+                        pes = driver.page_source
+                        page = pars(pes)
+                        if not page:
+                            continue
+                        if name_team == page[0]:
+                            data[0] += page[1]
+                            data[1] += page[2]
+                            data[2] += page[3]
+                            data[3] += page[4]
+                        else:
+                            data[0] += page[3]
+                            data[1] += page[4]
+                            data[2] += page[1]
+                            data[3] += page[2]
+                    print(data)
+        time.sleep(345600)
