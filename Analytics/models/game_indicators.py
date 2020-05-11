37e33083c43202773a10563c95934624c1690e10
August26 <velf.ats.26@gmail.com>
2020-05-11 15:05:05 +0300

    refactor params and edited structure

--- Analytics/models/game_indicators.py
+++ Analytics/models/game_indicators.py
@@ -1,29 +1,31 @@
 from .structure import Structure
+from types import MappingProxyType
 
 
 class TeamIndicators(Structure):
     """
-    :param name: [str] = Team name
-    :param chances: [int] = Actual win chance
-    :param g: [int] = Goals scored
-    :param xg: [float] = Expected goals
-    :param sh: [int] = Total hits
-    :param sh_target: [int] = Total hits on target
-    :param deep: [int] = Number of passes 18m from the opponent's goal
-    :param ppda: [float] = High pressure (not less than 40 meters from goal)
-    :param xpts: [float] = How many ops hit on goal % overall
-    :param players: [list] = Players in current match
+    :param name: Team name
+    :param chances: Actual win chance
+    :param g: Goals scored
+    :param xg: Expected goals
+    :param sh: Total hits
+    :param sh_target: Total hits on target
+    :param deep: Number of passes 18m from the opponent's goal
+    :param ppda: High pressure (not less than 40 meters from goal)
+    :param xpts: How many ops hit on goal % overall
+    :param ball_possession: % ball possession
+    :param players: Players in current match
     """
-    _fields = ["name", "chances", "g", "xg", "sh", "sh_target", "deep", "ppda", "xpts", "players"]
-    _type = [str, int, int, float, int, int, int, float, float, list]
+    _fields = MappingProxyType({"name": str, "chances": int, "g": int, "xg": float, "sh": int, "sh_target": int,
+                                "deep": int, "ppda": float, "xpts": float, "bp": int, "players": list})
 
 
 class GameIndicators(Structure):
     """
-    :param match: [str] = Match name # year_team1_result_team2 (2014_MyTeam_10_YourTeam)
-    :param chances_draw: [float] = Actual draw chance
-    :param team1: [dict] = All data from Team 1
-    :param team2: [dict] = All data from Team 2
+    :param match: Match name # year_team1_result_team2 (2014_MyTeam_10_YourTeam)
+    :param chances_draw: Actual draw chance
+    :param team1: All data from Team 1
+    :param team2: All data from Team 2
     """
-    _fields = ["match", "chances_draw", "team1", "team2"]
-    _type = [str, int, TeamIndicators().dict, TeamIndicators().dict]
+    _fields = MappingProxyType({"match": str, "chances_draw": int, "team1": TeamIndicators().dict,
+                                "team2": TeamIndicators().dict})
