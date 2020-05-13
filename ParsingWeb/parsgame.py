05c2eb23120a147668ca255240544649b72c4152
gogi-biker <biker92gogi@mail.ru>
2020-05-13 16:35:17 +0300

    убрал функцию и сократил код.

--- ParsingWeb/parsgame.py
+++ ParsingWeb/parsgame.py
@@ -1,81 +1,68 @@
-import re
-import time
+# Стягивает информацию о результатах матчей всех команд с сайта understat.com
 import logging
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from config import url, years, ligues
-from parsweb import lig
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+from ParsingWeb.parsweb import lig
+import sys
+sys.path.append("..\DataBase")
+from mongo_default import MongoDefault
+from Analytics.models.game_indicators import GameIndicators, TeamIndicators
 
 
-def pars(html):
-    """
-    :param html: - получает html код страницы .
-    :ball_possesion: - получает три значения changes .
-    :return: - возвращает список где первый элемент это название команды чьи значения указаны с левой стороны таблицы
-     2 и 3 пренадлежат все тожей команже а 4 и 5 команде которая с права . Последний элемент списка это список значений
-     changes.
-    """
-    soup = BeautifulSoup(html, 'html.parser')
-    indicators = []
-    name_team_home = soup.find('div', class_='progress-home progress-over').get_text()
-    if not name_team_home:
-        logging.warning('The name of the team that plays at home was not found.')
-        return None
-    indicators.append(name_team_home[1:-1])
-    values = (7, 9, 8, 10)
-    for value in values:
-        result = soup.find_all('div', class_='progress-value')[value].get_text()
-        if not result:
-            logging.warning('team game values not found.')
-            continue
-        if '.'in result:
-            indicators.append(float(result))
-        else:
-            indicators.append(int(result))
-    result = soup.find_all('div', class_='progress-bar')[1]
-    ball_possesion = re.findall(r'title="([\d]+)', str(result)) # пока не работаем с ними.
-    indicators.append(ball_possesion)
-    return indicators
+if __name__ == "__main__":
 
+    driver = webdriver.ChromeOptions()
+    driver.add_argument('headless')
+    driver.add_argument('window-size=1920x935')
+    driver = webdriver.Chrome(chrome_options=driver)
 
-if __name__ == '__main__':
+    for ligue in ligues:
+        teams = lig(ligue)
+        for year in years:
+            for team in teams:
+                http = url + team + '/' + year
+                driver.get(http)
 
-    while True:
-        for ligue in ligues:
-            teams = lig(ligue)
-            for year in years:
-                for team in teams:
-                    http = url + team + '/' + year
-                    driver = webdriver.ChromeOptions()
-                    driver.add_argument('headless')
-                    driver.add_argument('window-size=1920x935')
-                    driver = webdriver.Chrome(chrome_options=driver)
-                    driver.get(http)
-                    quantity = driver.find_elements_by_tag_name("a")
-                    links = tuple(x.get_attribute("href")for x in quantity if "match" in str(x.get_attribute("href")))
-                    if not links:
-                        logging.warning("not possible to find team matches")
-                        continue
-                    name_team = re.findall(r'team/([\w]+)', str(http))  # название домашней команды .
-                    data = [0, 0, 0, 0]
-                    for link in links:
-                        driver.get(link)
-                        driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[2]/div[1]/div/div[1]/div/label[3]'
-                                                     ).click()
-                        pes = driver.page_source
-                        page = pars(pes)
-                        if not page:
-                            continue
-                        if name_team == page[0]:
-                            data[0] += page[1]
-                            data[1] += page[2]
-                            data[2] += page[3]
-                            data[3] += page[4]
+                quantity = driver.find_elements_by_tag_name("a")
+                links = tuple(x.get_attribute("href") for x in quantity if "match" in str(x.get_attribute("href")))
+                if not links:
+                    logging.warning("not possible to find team matches")
+                    continue
+                for link in links:
+                    driver.get(link)
+                    driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[2]/div[1]/div/div[1]/div/label[3]'
+                                                 ).click()
+                    html = driver.page_source
+                    soup = BeautifulSoup(html, 'html.parser')
+                    team = TeamIndicators()
+                    team1 = {}
+                    team2 = {}
+                    draw = 0
+                    table = soup.find_all('div', class_='progress-bar')
+                    for item, ti in zip(table, team.dict().keys()):
+                        if 'CHANCES' in str(item.find_all('div', class_='progress-title')):
+                            data = tuple(x.get('title') for x in item.find_all('div')if x.get('title'))
+                            data = tuple(i[:-1] for i in data)
                         else:
-                            data[0] += page[3]
-                            data[1] += page[4]
-                            data[2] += page[1]
-                            data[3] += page[2]
-                    print(data)
-        time.sleep(345600)
+                            data = tuple(x.text for x in item.find_all('div', class_='progress-value'))
+                        team1[ti] = data[0]
+                        team2[ti] = data[-1]
+                        if len(data) == 3:
+                            draw = data[1]
+
+                    d = soup.find('tbody')
+                    team1['players'] = sum([[y.text for i, y in enumerate(x) if i == 1]
+                                            for x in d.find_all('tr')if "Sub" not in str(x)], [])
+
+                    driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[4]/div/div[1]/div/label[2]').click()
+                    page_plaers = driver.page_source
+                    page_plaers_way = BeautifulSoup(page_plaers, 'html.parser')
+                    plaers_wau = page_plaers_way.find('tbody')
+                    team2['players'] = sum([[y.text for i, y in enumerate(x) if i == 1]
+                                            for x in plaers_wau.find_all('tr') if "Sub" not in str(x)], [])
+
+                    match = f'{year}_{team1["name"]}_{team1["g"]}{team2["g"]}_{team2["name"]}'
+                    result = GameIndicators(match=match, chances_draw=draw, team1=team1, team2=team2)
+                    with MongoDefault('gameIndicators') as md:
+                        response = md.insertUpdate(match, result.dict())
