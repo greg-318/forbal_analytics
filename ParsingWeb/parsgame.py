77a352890fa4edd2c4d218a6743671da293e7b3b
gogi-biker <biker92gogi@mail.ru>
2020-05-16 20:55:32 +0300

    Изменил отправку в бд.

--- ParsingWeb/parsgame.py
+++ ParsingWeb/parsgame.py
@@ -8,7 +8,8 @@ import sys
 sys.path.append("..\DataBase")
 from mongo_default import MongoDefault
 from Analytics.models.game_indicators import GameIndicators, TeamIndicators
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s: %(message)s')
 
 
 def links(page):
@@ -18,7 +19,8 @@ def links(page):
     """
     driver.get(page)
     quantity = driver.find_elements_by_tag_name("a")
-    games = tuple(x.get_attribute("href") for x in quantity if "match" in str(x.get_attribute("href")))
+    games = tuple(x.get_attribute("href") for x in quantity
+                  if "match" in str(x.get_attribute("href")))
     if not games:
         return
     return games
@@ -32,7 +34,8 @@ def game(http):
     :return: draw - [str] показатель возможной ничьи  игры .
     """
     driver.get(http)
-    driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[2]/div[1]/div/div[1]/div/label[3]').click()
+    driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[2]'
+                                 '/div[1]/div/div[1]/div/label[3]').click()
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
     indicator = TeamIndicators()
@@ -42,9 +45,11 @@ def game(http):
     table = soup.find_all('div', class_='progress-bar')
     for item, ti in zip(table, indicator.dict().keys()):
         if 'CHANCES' in str(item.find_all('div', class_='progress-title')):
-            data = tuple(x.get('title')[:-1] for x in item.find_all('div')if x.get('title'))
+            data = tuple(x.get('title')[:-1]
+                         for x in item.find_all('div')if x.get('title'))
         else:
-            data = tuple(x.text for x in item.find_all('div', class_='progress-value'))
+            data = tuple(x.text for x in item.find_all
+            ('div', class_='progress-value'))
         team_one[ti] = data[0]
         team_duo[ti] = data[-1]
         if len(data) == 3:
@@ -55,18 +60,22 @@ def game(http):
 def players(url):
     """
     :param url: ссылка на страницу игры
-    :return: Возвращает два списка с теми игроками которые играли в данном матче одний команды и второй .
+    :return: Возвращает два списка с теми игроками которые играли
+    в данном матче одний команды и второй .
     """
     driver.get(url)
     html_players = driver.page_source
     soup = BeautifulSoup(html_players, 'html.parser')
     table = soup.find('tbody')
-    team_1 = sum([[y.text for i, y in enumerate(x) if i == 1] for x in table.find_all('tr') if "Sub" not in str(x)], [])
-    driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[4]/div/div[1]/div/label[2]').click()
+    team_1 = sum([[y.text for i, y in enumerate(x) if i == 1]
+                  for x in table.find_all('tr') if "Sub" not in str(x)], [])
+    driver.find_element_by_xpath('/html/body/div[1]/div[3]/div[4]'
+                                 '/div/div[1]/div/label[2]').click()
     html_Players1 = driver.page_source
     soup1 = BeautifulSoup(html_Players1, 'html.parser')
     table1 = soup1.find('tbody')
-    team_2 = sum([[y.text for i, y in enumerate(x) if i == 1] for x in table1.find_all('tr') if "Sub" not in str(x)], [])
+    team_2 = sum([[y.text for i, y in enumerate(x) if i == 1]
+                  for x in table1.find_all('tr') if "Sub" not in str(x)], [])
     return team_1, team_2
 
 
@@ -78,14 +87,11 @@ def insert( draw, team1, team2):
     :return: Результат отпраки в базу данных
     """
     match = f'{year}_{team1["name"]}_{team1["g"]}-{team2["g"]}_{team2["name"]}'
-    result = GameIndicators(match=match, chances_draw=draw, team1=team1, team2=team2)
+    result = GameIndicators(match=match, chances_draw=draw,
+                            team1=team1, team2=team2)
     with MongoDefault('gameIndicators') as md:
-        response = md.insertUpdate(match, result.dict())
-        if not response:
-            logging.warning(f'Something happened during insertion {match} into the database')
-            return False
-        else:
-            return response
+        response, _ = md.insertUpdate(value_uniq_key=match, value_to=result.dict())
+    return response
 
 
 if __name__ == "__main__":
@@ -98,12 +104,14 @@ if __name__ == "__main__":
         for ligue in ligues:
             teams = lig(ligue)
             for year in years:
-                for team in teams:
+                for counter, team in enumerate(teams):
                     http = url + team + '/' + year
-                    logging.info(f'page {http}')
+                    logging.info(f'page {http}  number of'
+                                 f' teams completed {counter}.')
                     links_page = links(http)
                     if not links:
-                        logging.warning(f"not possible to find team matches{http}")
+                        logging.warning(f"not possible to find "
+                                        f"team matches{http}")
                         continue
                     for link in links_page:
                         game_indicator = game(link)
@@ -116,6 +124,7 @@ if __name__ == "__main__":
                         status = insert(draw, team1, team2)
                         print(status)
                         if not status:
-                            logging.warning(f'Something happened during insertion {link} into the database')
-                            continue
+                            logging.warning(f'Something happened '
+                                            f'during insertion'
+                                            f' {link} into the database')
         time.sleep(345600)
