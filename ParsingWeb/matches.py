8077cfe6c4db8e1828c566ad44b4c2f658263e8b
August26 <velf.ats.26@gmail.com>
2020-06-10 20:48:02 +0300

    update parser matches

--- ParsingWeb/matches.py
+++ ParsingWeb/matches.py
@@ -2,6 +2,7 @@ import random
 import json
 import sys
 import logging
+import itertools
 sys.path.extend(["../Analytics/models/", "../DataBase/", "../Analytics/"])
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -10,11 +11,6 @@ from bs4 import BeautifulSoup
 import game_indicators as model
 from standard import check_standard_teams
 from config import json_matches, json_statistics, json_incidents, json_players
-
-
-def proxy_gen():
-    while True:
-        yield {"https": f"https://185.130.105.106:{random.randint(22000, 26000)}"}
 
 
 def periods_dict(all, first, second):
@@ -64,7 +60,10 @@ def get_seasons(link, proxy):
 
 def get_matches(url, proxy):
     try:
-        return requests.get(matches, proxies=proxy, verify=False, timeout=30).json()
+        result = requests.get(matches, proxies=proxy, verify=False, timeout=30)
+        if result.status_code != 200:
+            return
+        return result.json()
     except:
         return get_matches(url, next(proxies))
 
@@ -72,52 +71,53 @@ def get_matches(url, proxy):
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
     leagues = {"RFPL": "russia/premier-liga/203", "EPL": "england/premier-league/17", "La_liga": "spain/laliga/8",
-               "Bundesliga": "germany/bundesliga/35", "Serie_A": "italy/serie-a/23", "Ligue_1": "france/ligue-1/34"}
+                   "Bundesliga": "germany/bundesliga/35", "Serie_A": "italy/serie-a/23", "Ligue_1": "france/ligue-1/34"}
     link = "https://www.sofascore.com/tournament/football/"
-    proxies = proxy_gen()
+    proxies = (x for x in iter(lambda: {"https": f"https://185.130.105.106:{random.randint(22000, 26000)}"}, True))
 
     for league_key, league in leagues.items():
         logging.info(f'Starting {league_key}...')
         seasons_ids = get_seasons(link+league, next(proxies))
-
         for season in seasons_ids:
             year = '20'+season['year'].split("/")[1]
             if int(year) < 2014:
                 break
             logging.info(f'Season year - {year}')
-            matches = json_matches.replace("LEAGUE", str(league.split("/")[-1])).replace("ID", str(season["id"]))
-            response = get_matches(matches, next(proxies))
-            for values in response['teamEvents'].values():
-                for ids in values.values():
-                    for object_match in ids['total']:
-                        data_statistics, data_incidents, data_players = get_data(object_match["id"], next(proxies))
+            for i in itertools.count(start=31, step=1):
+                matches = json_matches.replace("LEAGUE_ID", str(league.split("/")[-1])).replace(
+                    "SEASON_ID", str(season["id"])).replace("NUM", str(i))
+                response = get_matches(matches, next(proxies))
+                if not response:
+                    break
+                for object_match in response['events']:
+                    data_statistics, data_incidents, data_players = get_data(object_match["id"], next(proxies))
 
-                        try:
-                            home_team = check_standard_teams(league_key, object_match['homeTeam']['name'])
-                            away_team = check_standard_teams(league_key, object_match['awayTeam']['name'])
-                        except:
-                            logging.warning(f'Error name team')
-                            with open('error_parsing_name.txt', 'a') as f:
-                                f.write(f'{data_statistics}\n{data_incidents}\n{data_players}\n{home_team}-{away_team}\n\n')
-                            continue
+                    try:
+                        home_team = check_standard_teams(league_key, object_match['homeTeam']['name'])
+                        away_team = check_standard_teams(league_key, object_match['awayTeam']['name'])
+                    except:
+                        logging.warning(f'Error name team')
+                        with open('error_parsing_name.txt', 'a') as f:
+                            f.write(f'{data_statistics}\n{data_incidents}\n{data_players}\n'
+                                    f'{object_match["homeTeam"]["name"]}-{object_match["awayTeam"]["name"]}\n\n')
+                        continue
 
+                    try:
                         home_result = data_incidents['incidents'][0]['homeScore']
                         away_result = data_incidents['incidents'][0]['awayScore']
-                        try:
-                            home_players = {player['player']['name']: player['statistics'] for player in data_players['home']['players'] if not player['substitute']}
-                            away_players = {player['player']['name']: player['statistics'] for player in data_players['away']['players'] if not player['substitute']}
-                        except:
-                            continue
-
+                        home_players = {player['player']['name']: player['statistics'] for player in data_players['home']['players'] if not player['substitute']}
+                        away_players = {player['player']['name']: player['statistics'] for player in data_players['away']['players'] if not player['substitute']}
                         period_all = sum([item['statisticsItems'] for item in data_statistics['statistics'][0]['groups']], [])
                         period_first = sum([item['statisticsItems'] for item in data_statistics['statistics'][1]['groups']], [])
                         period_second = sum([item['statisticsItems'] for item in data_statistics['statistics'][2]['groups']], [])
+                    except:
+                        continue
 
-                        periods = periods_dict(period_all, period_first, period_second)
+                    periods = periods_dict(period_all, period_first, period_second)
 
-                        m = model.GameIndicators(**periods)
-                        m.match = year+'_'+home_team+'_'+str(home_result)+str(away_result)+"_"+away_team
-                        m.players_home = {home_team: home_players}
-                        m.players_away = {away_team: away_players}
-                        m.sendToMongo()
-                        logging.info(f'Success wrote to Mongo')
+                    m = model.GameIndicators(**periods)
+                    m.match = year+'_'+home_team+'_'+str(home_result)+str(away_result)+"_"+away_team
+                    m.players_home = {home_team: home_players}
+                    m.players_away = {away_team: away_players}
+                    m.sendToMongo()
+                    logging.info(f'Success wrote to Mongo | year - {year}; league - {league_key}')
